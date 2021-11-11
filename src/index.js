cd8b58fd3058ad2433df1f69b07d088455cd680a
kezike <kezike13@gmail.com>
2021-11-11 02:06:06 -0500

    added profile pic and name

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const $rdf = require('rdflib');
 
 // Global variables
 const popupUri = 'popup.html';
-
 const SolidPG = SolidPG || {};
+const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
 
 //// BEGIN REST CONFIGURATION ////
 const getOptions = {
@@ -44,15 +44,15 @@ const bindKeyValue = (obj, key, val) => {
 // Bind events
 const bindEvents = () => {
   $(document).on('click', '#fetch-doc', loadDocument);
+  $(document).on('click', '#login', login);
+  $(document).on('click', '#logout', logout);
 };
 
 // Load content at target
 const loadDocument = async (event) => {
   event.preventDefault();
   const target = $('#fetch-doc-uri').val();
-  console.log(`Target: ${target}`);
   const loadResult = (await SolidPG.fetcher.load(target))['responseText'];
-  console.log(`Result: ${loadResult}`);
   const fetchedDoc = $('#fetched-doc');
   fetchedDoc.text(loadResult);
   fetchedDoc.removeClass('hidden');
@@ -63,30 +63,31 @@ const loadDocument = async (event) => {
 const loadProfile = (webId) => {
   SolidPG.fetcher.nowOrWhenFetched(webId, function(ok, body, xhr) {
     if (!ok) {
-      console.log("Oops, something happened and couldn't fetch data");
+      console.error('Error loading profile');
     } else {
       try {
         const me = $rdf.sym(webId);
         const name = FOAF('name');
         const myNameObj = SolidPG.fetcher.store.any(me, name);
-        const myName = myNameObj.value;
-        /*const img = FOAF('img');
-        const myImgObj = SolidPG.fetcher.store.any(me, img);
-        const myImg = myImgObj.value;*/
-        const knows = FOAF('knows');
-        const myFriends = SolidPG.fetcher.store.each(me, knows);
-        console.log('My name:', myName);
-        // console.log('My image:', myImg);
-        console.log('My friends:');
-        for (let i = 0; i < myFriends.length; i++) {
-          const friend = myFriends[i];
-          console.log(friend.value);
+        if (myNameObj) {
+          const myName = myNameObj.value;
+          const nameElement = $('#name');
+          nameElement.text(myName);
         }
-        document.getElementById('name').textContent = myName;
-        // document.getElementById('image').setAttribute('src', myImg);
-        document.getElementById('profile').classList.remove('hidden');
+        const img = FOAF('img');
+        const myImgObj = SolidPG.fetcher.store.any(me, img);
+        if (myImgObj) {
+          const myImg = myImgObj.value;
+          const imgElement = $('#image');
+          imgElement.attr('src', myImg);
+        } else {
+          const imgElement = $('#image');
+          imgElement.attr('src', 'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-person-512.png');
+        }
+        const profElement = $('#profile');
+        profElement.removeClass('hidden');
       } catch (err) {
-        console.error(err.name + ": " + err.message);
+        console.error('Error loading profile:', err);
       }
     }
   });
@@ -94,11 +95,18 @@ const loadProfile = (webId) => {
 
 // Login helper function
 const loginHelper = (session) => {
+  const loginButton = $('#login');
+  loginButton.attr('disabled', true);
+  loginButton.addClass('hidden');
+  const logoutButton = $('#logout');
+  loginButton.attr('disabled', false);
+  logoutButton.removeClass('hidden');
   const fetcher = $rdf.fetcher($rdf.graph());
   const updater = new $rdf.UpdateManager(fetcher.store);
   bindKeyValue(SolidPG, 'session', session);
   bindKeyValue(SolidPG, 'fetcher', fetcher);
   bindKeyValue(SolidPG, 'updater', updater);
+  loadProfile(session.webId);
 };
 
 // Login to app
@@ -118,17 +126,15 @@ const login = () => {
   });
 };
 
-const logout = () => {
-  // localStorage.removeItem("solid-auth-client");
-  // localStorage.clear();
-  return $auth.logout();
+const logout = async () => {
+  const logoutButton = $('#logout');
+  logoutButton.attr('disabled', true);
+  await $auth.logout();
+  const loginButton = $('#login');
+  loginButton.removeClass('hidden');
+  logoutButton.attr('disabled', false);
+  logoutButton.addClass('hidden');
 };
 //// END APP ////
 
 $(window).on('load', init);
-
-module.exports = {
-  login,
-  logout,
-  loadDocument
-};
